b4659c055ea8891206a1fed672c3eb12dbaa91d8
anguo <anguo@surpath.net>
2020-04-11 21:54:21 +0800

    git

--- src/pages/Count.jsx
+++ src/pages/Count.jsx
@@ -4,7 +4,7 @@ export default class Count extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            count: 1
         }
     }
 
