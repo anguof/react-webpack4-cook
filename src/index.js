59f45d075954fd4a67109333c1116d30a4e58391
anguo <anguo@surpath.net>
2020-04-11 13:52:29 +0800

    Second

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Router />
-    </BrowserRouter>,
-    document.getElementById("app")
-);
+/*初始化*/
+renderWithHotReload(Router);
+
+/*热更新*/
+if (module.hot) {
+    console.log(111)
+    module.hot.accept("./router/index.js", () => {
+        const Router = require("./router/index.js").default;
+        renderWithHotReload(Router);
+    });
+}
+
+function renderWithHotReload(Router) { //4、定义渲染函数
+    ReactDOM.render(
+        <AppContainer>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+        </AppContainer>,
+        document.getElementById("app")
+    );
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,36 @@
+const webpack = require('webpack');
 const path = require("path");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const PurifyCSS = require('purifycss-webpack')
+const glob = require('glob-all')
 
 module.exports = {
     mode: "development",
+    devtool: "cheap-module-eval-source-map",
     entry: ["./src/index.js"],
+    optimization: {
+        splitChunks: {
+            chunks: "all", // 所有的 chunks 代码公共的部分分离出来成为一个单独的文件
+        },
+        usedExports: true,
+    },
+    // entry: ["react-hot-loader/patch"],
     output: {
         // 输出目录
         path: path.join(__dirname, "dist"),
         // 文件名称
         filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+        alias: {
+            "@": path.join(__dirname, "src"),
+            pages: path.join(__dirname, "src/pages"),
+            router: path.join(__dirname, "src/router")
+        }
     },
     module: {
         rules: [
@@ -23,23 +44,60 @@ module.exports = {
                 ]
             },
             {
-                test: /\.s[ac]ss$/i,
+                test: /\.scss$/,
                 use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
+                    // "style-loader", // b不再需要style-loader要已经分离处理
+                    MiniCssExtractPlugin.loader,
+                    "css-loader", // 编译css
+                    "postcss-loader",
+                    "sass-loader" // 编译scss
+                ]
             },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        outputPath: 'images/', // 图片输出的路径
+                        limit: 10 * 1024
+                    }
+                }
+            },
+            {
+                test: /\.(eot|woff2?|ttf|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            name: '[name]-[hash:5].min.[ext]',
+                            limit: 5000, // fonts file size <= 5KB, use 'base64'; else, output svg file
+                            publicPath: 'fonts/',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
         ]
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                VUEP_BASE_URL: JSON.stringify('http://localhost:8080')
+            }
+        }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             filename: 'index.html', // 最终创建的文件名
             template: path.join(__dirname, 'src/template.html') // 指定模板路径
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery', // npm
+            jQuery: 'jQuery' // 本地Js文件
         })
     ],
     devServer: {
